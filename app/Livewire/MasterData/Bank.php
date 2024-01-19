53f8fd4efa1fdec49c1cffc8926768dc610fce32
fhmiibrhimdev <fahmidev.ibrahim@gmail.com>
2024-01-19 14:03:08 +0700

    feat: crud feature bank

--- app/Livewire/MasterData/Bank.php
+++ app/Livewire/MasterData/Bank.php
@@ -3,11 +3,130 @@
 namespace App\Livewire\MasterData;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\Bank as ModelsBank;
 
 class Bank extends Component
 {
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+    protected $listeners = [
+        'delete'
+    ];
+    protected $rules = [
+        'kode'       => 'required',
+        'nama_bank'  => 'required',
+    ];
+
+    public $lengthData = 5;
+    public $searchTerm;
+    public $previousSearchTerm = '';
+    public $isEditing = false;
+
+    public $dataId, $kode, $nama_bank;
+
+    public function updatingLengthData()
+    {
+        $this->resetPage();
+    }
+
+    private function searchResetPage()
+    {
+        if ($this->searchTerm !== $this->previousSearchTerm) {
+            $this->resetPage();
+        }
+    
+        $this->previousSearchTerm = $this->searchTerm;
+    }
+
     public function render()
     {
-        return view('livewire.master-data.bank');
+        $this->searchResetPage();
+        $search = '%'.$this->searchTerm.'%';
+
+        $data = ModelsBank::where('kode', 'LIKE', $search)
+                    ->orWhere('nama_bank', 'LIKE', $search)
+                    ->paginate($this->lengthData);
+
+        return view('livewire.master-data.bank', compact('data'))->title('Bank');
+    }
+
+    private function dispatchAlert($type, $message, $text)
+    {
+        $this->dispatch('swal:modal', [
+            'type'      => $type,  
+            'message'   => $message, 
+            'text'      => $text
+        ]);
+
+        $this->resetInputFields();
+    }
+
+    public function isEditingMode($mode)
+    {
+        $this->isEditing = $mode;
+    }
+
+    private function resetInputFields()
+    {
+        $this->kode      = '';
+        $this->nama_bank = '';
+    }
+
+    public function cancel()
+    {
+        $this->resetInputFields();
+    }
+
+    public function store()
+    {
+        $this->validate();
+
+        ModelsBank::create([
+            'kode'      => $this->kode,
+            'nama_bank' => $this->nama_bank,
+        ]);
+
+        $this->dispatchAlert('success', 'Success!', 'Data created successfully.');
+    }
+    
+    public function edit($id)
+    {
+        $this->isEditing = true;
+        $data = ModelsBank::findOrFail($id);
+        $this->dataId    = $id;
+        $this->kode      = $data->kode;
+        $this->nama_bank = $data->nama_bank;
+    }
+    
+    public function update()
+    {
+        $this->validate();
+        
+        if ($this->dataId) {
+            ModelsBank::findOrFail($this->dataId)->update([
+                'kode'      => $this->kode,
+                'nama_bank' => $this->nama_bank,
+            ]);
+
+            $this->dispatchAlert('success', 'Success!', 'Data updated successfully.');
+            $this->dataId = null;
+        }
+    }
+
+    public function deleteConfirm($id)
+    {
+        $this->dataId = $id;
+        $this->dispatch('swal:confirm', [
+            'type'      => 'warning',  
+            'message'   => 'Are you sure?', 
+            'text'      => 'If you delete the data, it cannot be restored!'
+        ]);
+    }
+
+    public function delete()
+    {
+        ModelsBank::findOrFail($this->dataId)->delete();
+        $this->dispatchAlert('success', 'Success!', 'Data deleted successfully.');
     }
 }
--- database/migrations/2024_01_13_093544_create_banks_table.php
+++ database/migrations/2024_01_13_093544_create_banks_table.php
@@ -11,8 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('banks', function (Blueprint $table) {
+        Schema::create('bank', function (Blueprint $table) {
             $table->id();
+            $table->text('kode');
+            $table->text('nama_bank');
             $table->timestamps();
         });
     }
@@ -22,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('banks');
+        Schema::dropIfExists('bank');
     }
 };
